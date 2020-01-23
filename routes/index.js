a4534fcd94fc6ce9c4e9db5a8b2d996e4cd09833
Akinpelumi <54557636+Akinpelumi@users.noreply.github.com>
2020-01-23 10:29:49 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,10 @@ router.get('/', function(req, res, next) {
 
 router.post('/login', controllerx.login );
 router.post('/signup', controllerx.signup );
-router.get('/total', controllerx.total );
-router.put('/updateuser/:id', controllerx.updateUser );
+
 router.post('/create', authorization, controllery.createPost );
 router.get('/allpost', authorization, controllery.getAllPost);
 router.get('/singlepost/:id', authorization, controllery.getParticularPost);
-router.put('/updatepost/:id', authorization, controllery.updatePost );
-router.delete('/delete/:id', authorization,  controllery.deletePost);
-
 
 
 module.exports = router;
